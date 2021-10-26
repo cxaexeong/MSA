dd2112d7f1ddd01a469bae2c1a827d9f26fa20ac
hjuuujh <hjuuujh@gmail.com>
2021-10-26 10:59:02 +0900

    11

--- back/api/settings.py
+++ back/api/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!5qmq=9e+wdf%xd&+vlhq+#p_x09!-^b@7&t1b^z&s0vvknwi5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -102,17 +102,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "vpdb",
-        "USER": "vpuser",
-        "PASSWORD": "1234",
-        "HOST": "3.15.69.19",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
         "PORT": "3306",
-        # "ENGINE": "django.db.backends.mysql",
-        # "NAME": "vpdb",
-        # "USER": "root",
-        # "PASSWORD": "12345",
-        # "HOST": "localhost",
-        # "PORT": "3306",
     }
 }
 
